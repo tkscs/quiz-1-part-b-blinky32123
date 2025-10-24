9fde39b1c6acd02c8b3bcd774ecd49afcad20756
blinky32123 <jkrepner2028@kehillahstudent.org>
2025-10-24 11:29:43 -0700

    test 1

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import turtle
 
 # YOUR CODE HERE
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(45)
+turtle.forward(71)
+turtle.right(90)
+turtle.forward(71)
+turtle.right(90)
+turtle.forward(71)
+turtle.right(90)
+turtle.forward(71)
+turtle.right(90)
 
 turtle.exitonclick()
